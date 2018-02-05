bad804d57ab405bc341acba3dda188f452a9f904
Sayed Abdo <sayedabdo@Sayeds-Mac.local>
2018-02-05 05:21:47 -0800

    The End OF implementation :)

---
+++ Pokedex/Util/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Sayed Abdo on 2/5/18.
+//  Copyright © 2018 sayedAbdo. All rights reserved.
+//
+
+import Foundation
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
+
---
+++ Pokedex/View/PokeCell.swift
@@ -0,0 +1,32 @@
+//
+//  Pokecell.swift
+//  Pokedex
+//
+//  Created by Sayed Abdo on 2/5/18.
+//  Copyright © 2018 sayedAbdo. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+}
